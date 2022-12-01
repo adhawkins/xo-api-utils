13b942ea34d0c522a9017a2299e75c7cb9ce9bd5
Andy Hawkins <andy@gently.org.uk>
2022-12-01 11:46:50 +0000

    Reformatted as per PEP8

--- xoAPI/xoAPI.py
+++ xoAPI/xoAPI.py
@@ -1,23 +1,24 @@
 import requests
 
+
 class xoAPI:
-	API_BASE="/rest/v0/"
+    API_BASE = "/rest/v0/"
 
-	def __init__(self, URL, token):
-		self.URL = URL
-		self.token = token
+    def __init__(self, URL, token):
+        self.URL = URL
+        self.token = token
 
-	def makeRequest(self, object):
-		cookies = {
-			"authenticationToken": self.token
-		}
+    def makeRequest(self, object):
+        cookies = {
+            "authenticationToken": self.token
+        }
 
-		if object.startswith(xoAPI.API_BASE):
-			url = f"{self.URL}{object}"
-		else:
-			url = f"{self.URL}{xoAPI.API_BASE}{object}"
+        if object.startswith(xoAPI.API_BASE):
+            url = f"{self.URL}{object}"
+        else:
+            url = f"{self.URL}{xoAPI.API_BASE}{object}"
 
-		ret = requests.get(url, cookies = cookies)
-		ret.raise_for_status()
-		return ret.json()
-
+        # print(f"Requesting: '{url}'")
+        ret = requests.get(url, cookies=cookies)
+        ret.raise_for_status()
+        return ret.json()

33f36e10e4b76acaeee74365393fc89e3b5e1177
Andy Hawkins <andy@gently.org.uk>
2023-04-10 20:29:07 +0100

    Include VMs not running, listing them under the pool

--- xo-report.py
+++ xo-report.py
@@ -17,6 +17,14 @@ def hostsInPool(hosts, poolId):
 
 	return ret
 
+def vmsInPool(vms, poolId):
+	ret = {}
+	for vm in vms.values():
+		if vm['$container']==poolId:
+			ret[vm['id']]=vm
+
+	return ret
+
 def vmsOnHost(vms, hostId):
 	ret = {}
 	for vm in vms.values():
@@ -24,6 +32,16 @@ def vmsOnHost(vms, hostId):
 			ret[vm['id']]=vm
 
 	return ret
+
+def printVM(vm, indent):
+	tab = '\t'
+	RAM = Quantity(vm['memory']['size'], 'B')
+	print(f"{tab * indent}VM: '{vm['name_label']}' - CPUs: {vm['CPUs']['number']}, RAM: {RAM:b}")
+	for vbd in vm['$VBDs']:
+		if vbd in vdis:
+			vdi = vdis[vbd]
+			size = Quantity(vdi['size'], 'B')
+			print(f"{tab * (indent + 1)}VDI: '{vdi['name_label']}' - {size:b}")
 
 parser = argparse.ArgumentParser(description='View VM snapshot info')
 
@@ -46,15 +64,12 @@ for vdi in rawvdis:
 for pool in pools.values():
 	print(f"Pool: {pool['name_label']}")
 
+	for vm in vmsInPool(vms, pool['id']).values():
+		printVM(vm, 1)
+
 	for host in hostsInPool(hosts, pool['id']).values():
 		RAM = Quantity(host['memory']['size'], 'B')
 		print(f"\tHost: {host['name_label']}, CPUs: {host['cpus']['cores']} cores, {host['cpus']['sockets']} sockets, RAM: {RAM:b}")
 
 		for vm in vmsOnHost(vms, host['id']).values():
-			RAM = Quantity(vm['memory']['size'], 'B')
-			print(f"\t\tVM: '{vm['name_label']}' - CPUs: {vm['CPUs']['number']}, RAM: {RAM:b}")
-			for vbd in vm['$VBDs']:
-				if vbd in vdis:
-					vdi = vdis[vbd]
-					size = Quantity(vdi['size'], 'B')
-					print(f"\t\t\tVDI: '{vdi['name_label']}' - {size:b}")
+			printVM(vm, 2)
